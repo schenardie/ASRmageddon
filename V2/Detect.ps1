803f5433304dc81ed8f8e0de20c3ea383fb33de6
Jose Schenardie <schenardie@gmail.com>
2023-01-17 17:10:05 +1100

    fix detect

--- V2/Detect.ps1
+++ V2/Detect.ps1
@@ -1,6 +1,6 @@
 try {
     $Programs = Invoke-RestMethod -Uri "https://raw.githubusercontent.com/schenardie/ASRmageddon/main/V2/AppList.json" -Headers @{"Cache-Control"="no-cache"}
-    $LastUpdated = ($Programs | Select-Object -First 1).LastUpdated
+    $LastUpdated = ($Programs | Select-Object -First 1).Fullname
     if (Test-Path $env:temp\ShortcutRepairs.log) {
         $LastRun = [regex]::Matches((Get-Content $env:temp\ShortcutRepairs.log -Tail 1) , '(?<=\[).+?(?=\])').Value
         if ( (Get-Date "$LastUpdated") -lt (Get-Date "$LastRun")) {
